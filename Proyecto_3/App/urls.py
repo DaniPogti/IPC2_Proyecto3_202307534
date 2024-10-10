608b6438bcbbb4fee1a4ac3139bb0d4e34a15684
Daniel Portillo <3648663810101@ingenieria.usac.edu.gt>
2024-10-09 21:14:08 -0600

    commit 3
    Base de l frontend

--- Proyecto_3/App/urls.py
+++ Proyecto_3/App/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('navbar/', views.navbar, name='navbar')
 ]
--- Proyecto_3/App/views.py
+++ Proyecto_3/App/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request): 
     return render(request, 'index.html')
+
+def navbar(request):
+    return render(request, 'navbar.html')
