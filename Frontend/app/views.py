f6e486356018378cf7d1e1ad51438606a8ad97ff
Daniel Portillo <3648663810101@ingenieria.usac.edu.gt>
2024-10-16 21:43:44 -0600

    Commit  6
    Estructura para mostrar el xml leido en backend

--- Frontend/app/views.py
+++ Frontend/app/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from .forms import FileForms
 import requests
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    ContenidoXml = None
+    return render(request, 'index.html', {'ContenidoXml': ContenidoXml})
 
 def mostrar(request):
-    return 'hola'
+    ContenidoXml = ""
+    
+    if request.method == 'POST':
+        form = FileForms(request.POST, request.FILES)
+        
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            ContenidoXml = file.read().decode('utf-8')
+        else:
+            print(form.errors)
+    return render(request, 'index.html', {'ContenidoXml': ContenidoXml})
+ 
