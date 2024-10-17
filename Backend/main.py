a2f9c8631d0db93cb3b4e7c71da6ae0977b1797e
Daniel Portillo <3648663810101@ingenieria.usac.edu.gt>
2024-10-16 20:46:28 -0600

    Commit 5
    Leer en el backend exitoso, puede leer varias empresas con varios servicios y puede hacer la consulta en thundetclient

--- Backend/main.py
+++ Backend/main.py
@@ -39,6 +39,20 @@ class Negativo:
 class Message():
     def __init__(self, mensaje):
         self.mensaje = mensaje
+        
+class Empresa():
+    def __init__(self, nombre, servicios):
+        self.nombre = nombre
+        self.servicios = servicios
+        
+class Servicio:
+    def __init__(self, nombre, alias):
+        self.nombre = nombre
+        self.alias = alias
+        
+class Alia():
+    def __init__(self, alias):
+        self.alias = alias
 
 @app.route('/')
 
@@ -59,6 +73,11 @@ def LeerXml():
         doc = minidom.parseString(xml_content)
         root = doc.documentElement
         
+        positive = [] 
+        negative = [] 
+        company = [] 
+        msj = []
+        
         diccionario = root.getElementsByTagName('diccionario')
         for dic in diccionario:
             positivos = dic.getElementsByTagName('sentimientos_positivos')
@@ -69,42 +88,57 @@ def LeerXml():
                 palabras = positivo.getElementsByTagName('palabra')
                 for pal in palabras:
                     good = pal.firstChild.nodeValue.strip()
-                    Positivos.append(good)
+                    positive.append(Positivo(good))
+                    #Positivos.append(good)
                     print(f"Positivo: {good}")
                     
             for negativo in negativos:
                 palabras = negativo.getElementsByTagName('palabra')
                 for pal in palabras:
                     bad = pal.firstChild.nodeValue.strip()
-                    Negativos.append(bad)
+                    negative.append(Negativo(bad))
+                    #Negativos.append(bad)
                     print(f"Negativo: {bad}")
             
             for emp in emprezaAnalizar:
                 empresa = emp.getElementsByTagName('empresa')
                 for emp in empresa:
                     nombre = emp.getElementsByTagName('nombre')
+                    listServicios = []
                     for nom in nombre:
                         name = nom.firstChild.nodeValue.strip()
                         print(f"Empresa: {name}")
                     servicio = emp.getElementsByTagName('servicio')
                     for ser in servicio:
                         servicios = ser.getAttribute('nombre')
+                        listAlias = []
                         print(f"Servicio: {servicios}")
                         alias = ser.getElementsByTagName('alias')
                         for ali in alias:
                             A_lias = ali.firstChild.nodeValue.strip()
-                            Alias.append(A_lias)
+                            listAlias.append(Alia(A_lias))
                             print(f"Alias: {A_lias}")
-                        Servicios.append(servicios)
-                    Empresas.append(name)
+                        listServicios.append(Servicio(servicios, listAlias))
+                    empre = Empresa(name, listServicios)
+                    company.append(empre)
+                    print(f"Empresa: {nombre}")
+                    
+        dictionary = Diccionario(positive, negative, company)
+        Diccionarios.append(dictionary)
         
         listaMensajes = root.getElementsByTagName('lista_mensajes')
         for listaM in listaMensajes:
             mensaje = listaM.getElementsByTagName('mensaje')
             for mens in mensaje:
                 txt = mens.firstChild.nodeValue.strip()
-                Mensajes.append(txt)
+                msj.append(Message(txt))
                 print(f"Mensaje: {txt}")
+                Mensajes.append(txt)
+                
+        Clasificaciones.append
+        Clasify= CLasificacion(dictionary, msj)
+        Clasificaciones.append(Clasify)
+        
                 
         return "XML leído correctamente e impreso en la consola"
     
@@ -113,7 +147,49 @@ def LeerXml():
 
 @app.route('/ConsultarDatos', methods=['GET'])
 def ConsultarD():
-    pass
+    try:
+        resultado = ""
+
+        # Recorrer la lista de Clasificaciones
+        for clasificacion in Clasificaciones:
+            resultado += "======== Clasificación ========\n"
+            
+            # recorre diccionario dentro de clasificación
+            diccionario = clasificacion.diccionario
+            resultado += "------- Diccionario -------\n"
+            
+            # Mostrar positivos
+            resultado += "-> Sentimientos Positivos:\n"
+            for pos in diccionario.positivos:
+                resultado += f"    {pos.palabra}\n"
+                
+            # Mostrar negativas
+            resultado += "-> Sentimientos Negativos:\n"
+            for neg in diccionario.negativos:
+                resultado += f"    {neg.palabra}\n"
+                
+            # Mostrar empresas y sus servicios
+            resultado += " -> Empresas:\n"
+            for empresa in diccionario.empresas:
+                resultado += f"    Nombre: {empresa.nombre}\n"
+                for servicio in empresa.servicios:
+                    resultado += f"      Servicio: {servicio.nombre}\n"
+                    for alias in servicio.alias:
+                        resultado += f"        Alias: {alias.alias}\n"
+            
+            # Acceder a los mensajes
+            resultado += "------- Lista de Mensajes -------\n"
+            for mensaje in clasificacion.listaMensajes:
+                resultado += f"*  {mensaje.mensaje}\n"
+
+        return resultado
+
+    except Exception as e:
+        return str(e)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
 
 @app.route('/ConsultarFecha', methods=['POST'])
 def ConsultarF():
